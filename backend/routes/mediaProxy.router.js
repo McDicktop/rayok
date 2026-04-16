60884c589528612d09833f22799a80bec0fa271b
Gleb Ovcharov <funlov@Glebs-Mac-mini.local>
2026-04-16 19:42:14 +0300

    media regexp fix

--- backend/routes/mediaProxy.router.js
+++ backend/routes/mediaProxy.router.js
@@ -1,21 +1,33 @@
 const Router = require("express");
 const router = new Router();
-const {s3, BUCKET} = require('../utils/s3');
-const {GetObjectCommand} = require("@aws-sdk/client-s3")
+const { s3, BUCKET } = require("../utils/s3");
+const { GetObjectCommand } = require("@aws-sdk/client-s3");
 
-router.get('/{*key}', async (req, res) => {
+router.get("/{*key}", async (req, res) => {
     const key = req.params.key; // cover/uuid.png
     try {
-        const command = new GetObjectCommand({ Bucket: BUCKET, Key: key});
+        const command = new GetObjectCommand({ Bucket: BUCKET, Key: key });
         const { Body, ContentType } = await s3.send(command);
 
-        res.setHeader("Content-Type", ContentType ?? "application/octet-stream");
+        res.setHeader(
+            "Content-Type",
+            ContentType ?? "application/octet-stream",
+        );
         res.setHeader("Cache-Control", "public, max-age=31536000");
 
-        Body.pipe(res);
-    } catch {
-        res.status(404).json({message: "Not found"});
+        if (Body instanceof require("stream").Readable) {
+            Body.pipe(res);
+        } else {
+            const chunks = [];
+            for await (const chunk of Body) {
+                chunks.push(chunk);
+            }
+            res.end(Buffer.concat(chunks));
+        }
+    } catch (e) {
+        console.log(e)
+        res.status(404).json({ message: "Not found" });
     }
-})
+});
 
 module.exports = router;
